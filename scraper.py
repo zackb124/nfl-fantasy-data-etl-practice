b6366f011c7d3165846750fccead8836fff3dac4
TzachiBakshi <zachiba1@gmail.com>
2023-03-21 16:11:34 +0200

    Update documentation for the scraper

--- scraper.py
+++ scraper.py
@@ -12,8 +12,11 @@ table_df = tables[0]
 
 table_df.columns = table_df.columns.droplevel(level=0)
 
+# choose the relevant columns from the table
 players_df = table_df.loc[:, ['Player', 'Tm','Age']]
 
+# clean the text from irrelevant symbols from the Player column
 players_df['Player'] = players_df['Player'].replace(r'[*+]', '', regex=True)
 
+# clean the Age column from rows that aren't numbers
 players_df = players_df[players_df['Age'].str.isdigit()]
