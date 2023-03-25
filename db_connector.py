5a25af24a8835b682f8a8e4d1f5ad1fbd345215c
TzachiBakshi <zachiba1@gmail.com>
2023-03-25 17:44:25 +0300

    Optimise the df building + delete the table creation on PSQL

--- db_connector.py
+++ db_connector.py
@@ -4,8 +4,5 @@ from sqlalchemy import create_engine
 # create a connection to the PostgreSQL database
 engine = create_engine('postgresql://postgres:databasetestwsc@localhost:5432/postgres')
 
-# build a new table on PostgreSQL
-engine.execute("CREATE TABLE nfl_fantasy_players (Player TEXT, Tm TEXT,Age INT, Datetime TIMESTAMP)")
-
 # write the DataFrame to the PostgreSQL table
 players_df.to_sql("nfl_fantasy_players", engine, if_exists="replace", index=False)
--- scraper.py
+++ scraper.py
@@ -12,4 +12,12 @@ table_df = tables[0]
 # print the data frame to verify that it has been created successfully
 table_df.columns = table_df.columns.droplevel(level=0)
 
-players_df = table_df.loc[:, ['Player', 'Tm','Age']].assign(Player=lambda x: x['Player'].replace(r'[*+]', '', regex=True)).loc[lambda x: x['Age'].str.isdigit()].assign(Timestamp=pd.to_datetime(dt.datetime.now()))
+players_df = (
+    table_df
+    .loc[:, ['Rk','Player', 'Tm','Age']]
+    .assign(Player=lambda x: x['Player'].replace(r'[*+]', '', regex=True))
+    .loc[lambda x: x['Age'].str.isdigit()]
+    .assign(Rk=lambda x: x['Rk'].astype(int),
+            Age=lambda x: x['Age'].astype(int),
+            Timestamp=pd.to_datetime(dt.datetime.now()))
+)
